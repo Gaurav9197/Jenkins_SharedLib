5ec738e9b983f5cd1a28e8565afae7b8fd37a1ba
gaurav_gorle <75519798+Gaurav9197@users.noreply.github.com>
2026-01-11 21:21:25 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,10 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def call() {
+    withCredentials([string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')]) {
+        dependencyCheck(
+            odcInstallation: 'OWASP',
+            additionalArguments: "--scan ./ --nvdApiKey=${NVD_API_KEY} --data /var/jenkins_home/dependency-check-data"
+        )
+    }
+
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
